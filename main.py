051333cbe128507027fc782f684dfebc6ea83600
Nilrem <nilremnilrem@gmail.com>
2023-12-27 15:59:39 -0800

    allow switching between dictionaries

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import nearest
 import genetic_algorithm
 
 def main():
+    selected_dictionary = int(input("""
+    === Please Select Your Dictionary ===
+    1. 1000 most common English words
+    2. Official Scrabble Dictionary
+    """))
+
     selected_method = int(input("""
     === Please Select Method ===
     1. Brute Force
@@ -11,16 +17,20 @@ def main():
     3. Genetic Algorithm
     """))
     alphabet="abcdefghijklmnopqrstuvwxyz"
-    words = wordlist.get_wordlist_from_file()
+    wordlistfilenames = {
+        1: "words.txt",
+        2: "scrabble.txt"
+    }
+    words = wordlist.get_wordlist_from_file(name=wordlistfilenames[selected_dictionary])
 
     if selected_method == 1:
         solution, solutionwords = bruteforce.find_optimal_letters(alphabet, words)
         print(solution, "\n\n", solutionwords)
     elif selected_method == 2:
         solution, solutionwords, maxes=nearest.find_good_letters(alphabet, words)
-        print(solution, "\n", maxes, "\n\n", solutionwords)
+        print(solutionwords, "\n", maxes, "\n\n", solution)
     elif selected_method == 3:
-        best_letters = genetic_algorithm.do_genetic_algorithm(words, 1000, 1000)
+        best_letters = genetic_algorithm.do_genetic_algorithm(words, 100, 10000)
         print(best_letters)
 
     input("Press enter to close...")
