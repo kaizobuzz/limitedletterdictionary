c01c9ab39979ea49c50349d247edfc52af8ae915
Nilrem <nilremnilrem@gmail.com>
2023-12-27 13:06:51 -0800

    move functions to wordlist.py

---
+++ wordlist.py
@@ -0,0 +1,19 @@
+
+def get_wordlist_from_file(name="words.txt"):
+    words = []
+    with open(name, "r") as f:
+        while (x := f.readline()) != "":
+            words.append(x.strip())
+        return words
+
+def deduplicate_word_list(words):
+    new_words = {}
+    for word in words:
+        if simplify_word(word) in new_words:
+            new_words[simplify(word)] += 1
+        else:
+            new_words[simplify(word)] = 1
+    return new_words
+
+def simplify_word(word):
+    return "".join(set(word)).sort()
