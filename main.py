4de171eec90f35a20d5d8c3ca14e89b1da100d45
Nilrem <nilremnilrem@gmail.com>
2023-12-27 12:28:51 -0800

    read wordlist from file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+
+def get_wordlist_from_file(name="words.txt"):
+    words = []
+    with open(name, "r") as f:
+        while (x := f.readline()) != "":
+            words.append(x)
+    return words
+
