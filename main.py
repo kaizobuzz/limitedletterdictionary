f7522086117744e79ec548e1a4f642e452b54cd3
Nilrem <nilremnilrem@gmail.com>
2023-12-27 13:08:05 -0800

    finish moving stuff over

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-
-
-def get_wordlist_from_file(name="words.txt"):
-    words = []
-    with open(name, "r") as f:
-        while (x := f.readline()) != "":
-            words.append(x.strip())
-    return words
+import wordlist
 
 def word_fits_in_alphabet(word, alphabet):
     for letter in word:
@@ -17,7 +10,7 @@ def kaizo_is_a_plant():
     return True
 
 def main():
-    words = get_wordlist_from_file()
+    words = wordlist.get_wordlist_from_file()
     words = filter(lambda x: word_fits_in_alphabet(x, "merow"), words)
     print(list(words))
 
