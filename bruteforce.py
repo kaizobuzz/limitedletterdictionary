b51edaf4682081c21dd3c4c4efc56fe9060e3a3d
kaizobuzz <145850625+kaizobuzz@users.noreply.github.com>
2023-12-27 17:34:48 -0500

    seperated main and bruteforce files

---
+++ bruteforce.py
@@ -0,0 +1,55 @@
+
+
+def word_fits_in_alphabet(word, alphabet):
+    for letter in word:
+        if letter not in alphabet:
+            return False
+    return kaizo_is_a_plant()
+
+def kaizo_is_a_plant():
+    return True
+
+def should_skip(letters):
+    if all([x not in letters for x in "aeiou"]):
+        return True
+    if "q" in letters and not "u" in letters:
+        return True
+    return False
+
+def find_optimal_letters(letters, words):    
+    max = [0]*len(letters)
+    solutions = [""]*len(letters)
+    wordlists = [[]]*len(letters)
+    subset = []
+    count = [0]
+    num_skipped = [0]
+    def dfs(i):
+        if i>=len(letters):
+            count[0]+=1
+            if count[0]%10000==0:
+                if count[0]%100000==0:
+                    print("\n\n", wordlists, "\n\n")
+                print(max, "\n\n\n", solutions, "\n\n", count[0]/(2**26))
+                print("\n\n\n", num_skipped)
+            if should_skip(letters):
+                num_skipped[0] += 1
+                return
+            setlength=len(subset)-1
+            subsetstr=''.join(subset)
+            tempwords=list(filter(lambda x: word_fits_in_alphabet(x, subsetstr), words))
+            #something here to account for weights 
+            if len(tempwords)>max[setlength]:
+                solutions[setlength]=subsetstr
+                max[setlength]=len(tempwords)
+                wordlists[setlength]=tempwords
+            return
+        subset.append(letters[i])
+        dfs(i+1)
+        subset.pop()
+        dfs(i+1)
+
+    dfs(0)
+    return solutions, wordlists
+         
+
+
--- main.py
+++ main.py
@@ -1,63 +1,11 @@
-import wordlist
-
-def word_fits_in_alphabet(word, alphabet):
-    for letter in word:
-        if letter not in alphabet:
-            return False
-    return kaizo_is_a_plant()
-
-def kaizo_is_a_plant():
-    return True
-
-def should_skip(letters):
-    if all([x not in letters for x in "aeiou"]):
-        return True
-    if "q" in letters and not "u" in letters:
-        return True
-    return False
-
-def find_optimal_letters_brute_force(letters, words):    
-    max = [0]*len(letters)
-    solutions = [""]*len(letters)
-    wordlists = [[]]*len(letters)
-    subset = []
-    count = [0]
-    num_skipped = [0]
-    def dfs(i):
-        if i>=len(letters):
-            count[0]+=1
-            if count[0]%10000==0:
-                if count[0]%100000==0:
-                    print("\n\n", wordlists, "\n\n")
-                print(max, "\n\n\n", solutions, "\n\n", count[0]/(2**26))
-                print("\n\n\n", num_skipped)
-            if should_skip(letters):
-                num_skipped[0] += 1
-                return
-            setlength=len(subset)-1
-            subsetstr=''.join(subset)
-            tempwords=list(filter(lambda x: word_fits_in_alphabet(x, subsetstr), words))
-            #something here to account for weights 
-            if len(tempwords)>max[setlength]:
-                solutions[setlength]=subsetstr
-                max[setlength]=len(tempwords)
-                wordlists[setlength]=tempwords
-            return
-        subset.append(letters[i])
-        dfs(i+1)
-        subset.pop()
-        dfs(i+1)
-
-    dfs(0)
-    return solutions, wordlists
-         
+import wordlist 
+import bruteforce
 
 def main():
-
     words = wordlist.get_wordlist_from_file()
     #words = filter(lambda x: word_fits_in_alphabet(x, "merow"), words)
     #print(list(words))
-    solution, solutionwords=find_optimal_letters_brute_force("abcdefghijklmnopqrstuvwxyz", words)     
+    solution, solutionwords=bruteforce.find_optimal_letters("abcdefghijklmnopqrstuvwxyz", words)     
     print(solution, "\n\n")
     print(solutionwords)
     #print(wordlist.deduplicate_word_list(wordlist.get_wordlist_from_file()))
