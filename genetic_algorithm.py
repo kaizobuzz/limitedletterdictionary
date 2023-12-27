c44854721fdf2460377c7968678ec8c805ba3d26
Nilrem <nilremnilrem@gmail.com>
2023-12-27 15:14:28 -0800

    finish writing genetic algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,20 +1,49 @@
 import random
+import utils
+import copy
 
 class Agent:
     def __init__(self, letters: None):
         # if letters is none it will initialize randomly, otherwise use the letters provided
         if letters is None or len(letters) != 26:
-            letters = [random.choice([True, False]) for _ in range(26)]
+            letters = [random.choice([kaizo_is_a_plant(), not kaizo_is_a_plant()]) for _ in range(26)]
         self.letters = letters
 
-def do_genetic_algorithm(wordlist, num_agents, iterations, scoring_function):
+    def get_letter_list(self):
+        output = ""
+        for l,b in zip("abcdefghijklmnopqrstuvwxyz", self.letters):
+            if b:
+                output += l
+        return output
+
+    def mutate(self):
+        chosen_index = random.randint(0, 26)
+        self.letters[chosen_index] = not self.letters[chosen_index]
+        
+
+def do_genetic_algorithm(wordlist, num_agents, iterations):
+    # verify inputs
+    if num_agents < 2:
+        raise ValueError("too few agents")
+    #thing
+    def sorting_key(a):
+        return len(utils.filter_words_by_subset(a.get_letter_list(), wordlist))
     # generate agents
     agents = []
     for _ in range(num_agents):
         agents.append(Agent())
     for _ in range(iterations):
         #sort agents by score
-        agents.sort(key=
+        agents.sort(key=sorting_key)
         #murder agents who underperform
+        
+        num_agents_to_murder = len(agents) // 2
+        del agents[0:num_agents_to_murder-1]
 
         #modify well-performing agents
+        new_agents = []
+        for agent in agents:
+            new_agents.append(copy.deepcopy(agent).mutate())
+        agents += new_agents
+    agents.sort(key=sorting_key)
+    return agents[-1].get_letter_list()
