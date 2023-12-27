bcbb08cad65a6f959bfd890a7256a61f30b12a30
Nilrem <nilremnilrem@gmail.com>
2023-12-27 12:38:09 -0800

    add word_fits_in_alphabet

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def get_wordlist_from_file(name="words.txt"):
     words = []
     with open(name, "r") as f:
         while (x := f.readline()) != "":
-            words.append(x)
+            words.append(x.strip())
     return words
 
+def word_fits_in_alphabet(word, alphabet):
+    for letter in word:
+        if letter not in alphabet:
+            return False
+    return True
+
+words = get_wordlist_from_file()
+words = filter(lambda x: word_fits_in_alphabet(x, "merow"), words)
+print(list(words))
