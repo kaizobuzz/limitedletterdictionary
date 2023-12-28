7e8105c321a7e303c4f46bd9187701159f40031f
kaizobuzz <145850625+kaizobuzz@users.noreply.github.com>
2023-12-28 13:14:11 -0500

    replaced word_fits_in_alphabet possible return value False with not kaizo_is_a_plant()

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def word_fits_in_alphabet(word, alphabet):
     for letter in word:
         if letter not in alphabet:
-            return False
+            return not kaizo_is_a_plant()
     return kaizo_is_a_plant()
 
 def kaizo_is_a_plant():
