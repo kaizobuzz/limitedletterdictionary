5137aae1fadad8af751ca161d51f0d1779c7e8ee
kaizobuzz <145850625+kaizobuzz@users.noreply.github.com>
2023-12-29 13:11:35 -0500

    added boundschecking for getting number inputs and added an option to view the wordlist of a subset after it's created

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     1. 1000 most common English words
     2. Official English Scrabble Dictionary
     3. Official French Scrabble Dictionary
-    """)
+    """, 1, 3)
 
     selected_method = utils.int_as_input("""
     === Please Select Method ===
@@ -20,7 +20,7 @@ def main():
     2. Nearest Neighbor Search
     3. Genetic Algorithm
     4. Genetic Algorithm (Fixed Size Version)
-    """)
+    """, 1, 4)
     alphabet="abcdefghijklmnopqrstuvwxyz"
     wordlistfilenames = {
         1: "words.txt",
@@ -32,6 +32,7 @@ def main():
     if selected_method == 1:
         solution, maxes, averages = bruteforce.find_optimal_letters(alphabet, bitfields)
         print(f"Num of words: {maxes}\n\nScore: {averages}\n\nSolutions: {solution}")
+        utils.ask_for_wordlist_number(solution, words)
     elif selected_method == 2:
         solution, maxes, averages=nearest.find_good_letters(alphabet, bitfields)
         #print(solutionwords, "\n") 
@@ -59,16 +60,17 @@ def main():
             combinedaverages[i]=combinedmaxes[i]/(i+1)
         print(f"Time taken: {time.time()-start_time}s\n")
         print(f"Combined solution:\nNum of words: {combinedmaxes}\n\nScore: {combinedaverages}\n\n Subsets: {combinedsolution}\n\n")
+        utils.ask_for_wordlist_number(combinedsolution, words)
     elif selected_method == 3:
         num_agents=100
         iterations=10000
         best_letters = genetic_algorithm.do_genetic_algorithm(bitfields, num_agents, iterations)
         print(best_letters)
-    
+        utils.ask_for_wordlist_yn(best_letters, words)
     elif selected_method == 4:
         num_agents = 100
         iterations = 100
-        size = utils.int_as_input("Choose the size of the set to be generated\n")
+        size = utils.int_as_input("Choose the size of the set to be generated\n", 1, 26)
         letter_list, score = genetic_algorithm_fixed_size.do_genetic_algorithm(bitfields, num_agents, iterations, size)
         num_words=score*size
         print(letter_list)
@@ -78,6 +80,7 @@ def main():
                 list(letter_list), num_words, list(alphabet), bitfields)
         print(f"Time taken: {time.time()-start_time}s\n")
         print(f"Letter list: {letter_list}\nNum of words: {num_words}\nScore: {score}\n")
+        utils.ask_for_wordlist_yn(letter_list, words)
     input("Press enter to close...")
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def kaizo_is_a_plant():
 
 def filter_words_by_subset(subset ,words):
     return list(filter(lambda x: word_fits_in_alphabet(x, subset), words))
+
 def check_against_bitfields(subset, bitfields):
     subsetbits=0
     count=0
@@ -18,6 +19,7 @@ def check_against_bitfields(subset, bitfields):
         if bitfield&~subsetbits==0:
             count+=1 
     return count
+
 def get_num_of_words_by_subset_with_cache(cache, subset, bitfields):
     found, val=cache.find(subset)
     if found:
@@ -32,7 +34,15 @@ def get_num_of_words_by_subset_with_cache(cache, subset, bitfields):
     cache.addnode(subset, val)
     return val
 
-
+def ask_for_wordlist_yn(subset, words):
+    if yes_no_input(f"Do you want to get the wordlist of subset {subset} (y/n)\n")==True:
+        print(filter_words_by_subset(subset, words))
+def ask_for_wordlist_number(subsets, words):
+    x=int_as_input("""what subset do you want to see the wordlist of, (input is number of letters in subset)
+                    (you can enter 0 if you dont want any)\n""", 0, len(subsets))
+    if x==0:
+        return
+    print(filter_words_by_subset(subsets[x-1], words))
 
 class Node:
     def __init__(self, key, val):
@@ -79,15 +89,28 @@ class LRUCache:
             del self.cache[deadnode.key]
             self.size-=1
 
-def int_as_input(message):
+def int_as_input(message, lower_bound=-1000000, upper_bound=1000000):
     x = 0
     while True:
         try: 
             x = int(input(message))
-            break
+            if x>=lower_bound and x<=upper_bound:
+                break
+            else:
+                print(f"Input does not fit in between lower bound {lower_bound} and upper bound {upper_bound}, try again \n")
         except ValueError:
             print("That wasn't a valid integer, try again \n")
     return x
+
+def yes_no_input(message):
+    while True:
+        user_input=input(message)
+        if user_input=='y' or user_input=='yes':
+            return True 
+        elif user_input=='n' or user_input=='no':
+            return False
+        else:
+            print("Invalid input, please use \'y\' or \'n\'")
 
 def alphabet(): 
     return "abcdefghijklmnopqrstuvwxyz"
