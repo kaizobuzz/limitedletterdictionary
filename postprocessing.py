dcc494bce9795eb2183ea17b9ccc338b587134ed
kaizobuzz <145850625+kaizobuzz@users.noreply.github.com>
2023-12-29 23:12:45 -0500

    made an automatic return with 26 letters

--- postprocessing.py
+++ postprocessing.py
@@ -1,5 +1,6 @@
 #TODO
 import utils
+import time
 
 def fore_minus_three_opt(subset, max, letters, bitfields):
     letterscopy=letters
@@ -14,3 +15,22 @@ def fore_minus_three_opt(subset, max, letters, bitfields):
                 return fore_minus_three_opt(subset, count, letters, bitfields)
         subset[i]=s_letter
     return ''.join(subset), max
+
+def start_processing(subsets, num_words, scores, letters, bitfields):
+    print("Starting postprocessing... doing 1_opt switch")
+    start_time=time.time()
+    end_message=f"Time taken: {time.time()-start_time}s\n"
+    if type(subsets) is list:
+        for i in range(len(subsets)):
+            subsets[i], num_words[i]=fore_minus_three_opt(
+            list(subsets[i]), num_words[i], list(letters), bitfields)
+            scores[i]=num_words[i]/(i+1)
+        print(end_message)
+        return subsets, num_words, scores
+    subsets, num_words=fore_minus_three_opt(
+    list(subsets), num_words, list(letters), bitfields)
+    scores=num_words/len(subsets)
+    print(end_message)
+    return subsets, num_words, scores
+             
+
